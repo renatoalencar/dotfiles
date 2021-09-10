b060c6e9907136ad025e3b8cd982b3eb71f62f71
Renato Alencar <renatoalencar.73@gmail.com>
2021-09-10 07:13:50 -0300

    Fuzzy finder, Python LSP with Pyright and more.

--- my-custom-things.el
+++ my-custom-things.el
@@ -26,13 +26,23 @@
                  typescript-mode
                  neotree
                  dracula-theme
-                 solarized-theme)))
+                 solarized-theme
+                 tuareg
+                 lsp-mode
+                 lsp-ui
+                 lsp-pyright
+                 git-gutter
+                 fuzzy-finder)))
 
 (defun add-melpa-to-package-archives ()
+  "Add melpa.org to package lists."
+
   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
   (package-initialize))
 
 (defun install-missing-packages (package-list)
+  "Install packages from PACKAGE-LIST that aren't installed."
+
   (add-melpa-to-package-archives)
   (package-refresh-contents)
 
@@ -41,14 +51,41 @@
       (package-install package))))
 
 (defun require-packages ()
+  "Require needed runtime packages."
+
   (require 'neotree)
   (require 'typescript-mode)
   (require 'smart-mode-line)
   (require 'yaml-mode)
   (require 'doom-modeline)
-  (require 'flycheck-clj-kondo))
+  (require 'flycheck-clj-kondo)
+  (require 'lsp-mode))
+
+(defun basename (name)
+  "Get a file or directory name base path from NAME.
+
+   Example:
+   (basename \"/tmp/foo\")
+   ;; \"/tmp/\"
+
+   (basename \"/tmp/bar/\")
+   ;; \"/tmp/\"
+  "
+
+  (let ((filename (if (directory-name-p name)
+                   (directory-file-name name)
+                   name)))
+    (file-name-directory filename)))
+
+(defun find-git-directory (directory)
+  "Goes recursively upwards on the directory tree until it finds a
+   .git directory and returns its basename."
+  (cond ((string= directory "/") nil)
+        ((file-exists-p (concat directory ".git")) directory)
+        (t (find-git-directory (basename directory)))))
 
 (defun setup-theme ()
+  "Setup themes editor and neotree."
 
   ;; setup dracula theme for both editor and emacs in general
   ;; now its solarized light
@@ -69,6 +106,8 @@
   (doom-modeline-mode 1))
 
 (defun setup-editor-behavior ()
+  "Setup editor behavior,like identation and global modes."
+
   (setq make-backup-files nil)
   (global-display-line-numbers-mode)
   (setq-default indent-tabs-mode nil)
@@ -77,6 +116,10 @@
   (tool-bar-mode -1)
 
   (global-flycheck-mode)
+  (global-git-gutter-mode +1)
+
+  (add-hook 'text-mode-hook 'flyspell-mode)
+  (add-hook 'prog-mode-hook 'flyspell-prog-mode)
 
   (if (not (display-graphic-p))
     (xterm-mouse-mode)))
@@ -97,17 +140,41 @@
   (local-set-key [mouse-9] #'tide-jump-to-definition)
   (local-set-key [mouse-8] #'tide-jump-back))
 
+(defun setup-python ()
+  "Setup python configuration, like LSP and more."
+
+  (require 'lsp-pyright)
+  (lsp-deferred))
+
+(defun custom-fuzzy-finder ()
+  "Run fuzzy-finder at the root at the project."
+
+  (fuzzy-finder :directory
+                (or (find-git-directory default-directory)
+                    default-directory)))
+
+(defun setup-fuzzy-finder ()
+  "Setup executable and configuration for fuzzy finder."
+
+  (setq fuzzy-finder-executable-path "~/.fzf/bin/fzf"))
+
 (defun setup-key-bindings ()
+  "Setup several custom key bindings."
+
   (global-set-key (kbd "C-x g") 'magit-status)
   (global-set-key (kbd "C-x |") #'split-window-right)
   (global-set-key (kbd "C-x -") #'split-window-below)
   (global-set-key (kbd "C-x C-1") 'neotree-toggle)
-  (global-set-key (kbd "C-x C-d") 'neotree-dir))
+  (global-set-key (kbd "C-x C-d") 'neotree-dir)
+  (global-set-key (kbd "C-x p") #'custom-fuzzy-finder))
 
 (defun setup-hooks ()
+  "Add hooks for several modes."
+
   (add-hook 'before-save-hook 'tide-format-before-save)
   (add-hook 'typescript-mode-hook #'setup-tide-mode)
-  (add-hook 'server-switch-mode #'raise-frame))
+  (add-hook 'server-switch-mode #'raise-frame)
+  (add-hook 'python-mode-hook #'setup-python))
 
 (defun setup-slime ()
   (setq inferior-lisp-program (executable-find "sbcl")))
